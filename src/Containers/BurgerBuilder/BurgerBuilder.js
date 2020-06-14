aabebfc6700babccdd7670ba048c7562e11bbd16
dp160610 <46530061+dp160610@users.noreply.github.com>
2020-06-14 11:43:05 +0530

    files are untracked

---
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import Aux1 from '../../hoc/Aux1';
+
+class BurgerBuilder extends Component{
+    render(){
+        return(
+                <div>
+<Aux1>
+<div>Burger</div>
+<div>Bulid Controls</div>
+</Aux1>
+                </div>
+        );
+    }
+}
+
+export default BurgerBuilder;
